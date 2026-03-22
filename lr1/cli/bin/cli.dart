f2a4a95df1acd1a7a3e0c8a8c26fd3c8e6c66851
ps_tsvetkov_i <tsvetkov-vanechka@inbox.ru>
2026-03-22 14:48:07 +0300

    feat: обработка арумента search
    проверка на пустоту списка аргументов и пустоту агрумента search

--- lr1/cli/bin/cli.dart
+++ lr1/cli/bin/cli.dart
@@ -1,8 +1,34 @@
+import 'dart:io';
+
 const version = '0.0.1'; 
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+   if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else 
+    articleTitle = arguments.join(' ');
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
 
 void main(List<String> arguments) {
   if (arguments.isEmpty)
     print('Hello, Dart!');
   else if (arguments.first == 'version')
     print('Version $version');
+  else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else 
+    printUsage();
 }
