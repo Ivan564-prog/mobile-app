b20d6d0b2a7ec6f3d4e6905a95260b113721a54d
ps_tsvetkov_i <tsvetkov-vanechka@inbox.ru>
2026-04-02 11:13:57 +0300

    feat(ui): создание класса тайла и вы

--- lr2/birdle/lib/main.dart
+++ lr2/birdle/lib/main.dart
@@ -4,15 +4,59 @@ void main() {
   runApp(const MainApp());
 }
 
+enum HitType {
+  hit,
+  partial,
+  miss,
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Tile('A', HitType.hit),     // зелёный
+              SizedBox(width: 8),
+              Tile('A', HitType.partial), // жёлтый
+              SizedBox(width: 8),
+              Tile('A', HitType.miss),    // серый
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
     );
